63f9b5644dbb6ff91981e2d3b20a6fe2f6969aa5
BloodyEagle <bloodyeagle@gmail.com>
2024-06-29 15:03:12 +0300

    add Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
-import {Component, OnInit} from '@angular/core';
+import {
+  Component
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  title = 'First-evo-project';
-  ngOnInit(): void {
-    throw new Error('Method not implemented!')
-  }
+export class AppComponent{
+  title = 'Hello';
 }
