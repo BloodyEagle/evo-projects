2d93dae1739f348285e8699b83d50203904adb0d
BloodyEagle <bloodyeagle@gmail.com>
2024-07-14 00:35:31 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Notify} from 'notiflix/build/notiflix-notify-aio';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'First-evo-project';
+  alerts: any[] = [
+    {
+      type: 'success',
+      msg: `Wery good!.`,
+    },
+    {
+      type: 'info',
+      msg: `Info from Notiflix.`,
+    },
+    {
+      type: 'danger',
+      msg: `Warning. Dangerous situation.`,
+    },
+    {
+      type: 'warning',
+      msg: `Warning. Dangerous situation.`,
+    },
+    {
+      type: 'error',
+      msg: `Warning. Dangerous situation.`,
+    },
+  ];
+
+  alert(type: string) {
+    let finded = this.alerts.find(item => item.type == type) ?? {type: 'error'};
+    switch (finded.type) {
+      case 'success':
+        Notify.success('Success Alert');
+        break;
+      case 'warning':
+        Notify.warning('Warning Alert');
+        break;
+      case 'info':
+        Notify.info('Info Alert');
+        break;
+      default:
+        Notify.failure('Error Alert');
+        break;
+    }
+  }
 }
