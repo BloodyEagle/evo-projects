2ae68fec88d2fe47708d44ca69c9de5606a6460e
BloodyEagle <bloodyeagle@gmail.com>
2024-07-16 14:02:03 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import {ResponseService} from "./response.service";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor(public response: ResponseService) {
-  }
-
-  ngOnInit() {
-    console.log('data => ', this.response.data);
-  }
+export class AppComponent {
+  title = 'First-evo-project';
 }
--- src/app/interfaces/response-server.ts
+++
@@ -1,6 +0,0 @@
-export interface ResponseServer {
-  userId: number,
-  id: number,
-  title: string,
-  completed: boolean
-}
