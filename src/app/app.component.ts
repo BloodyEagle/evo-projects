a8dae9106f9272ea72ad1c5a8828673eb904215e
BloodyEagle <bloodyeagle@gmail.com>
2024-07-17 14:49:39 +0300

    initial commit Form validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Book} from "./interfaces/book";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'First-evo-project';
+  new_book: Book = {
+    title: '',
+    author: ''
+  };
+
+  books: Book[] = [
+    {title: 'Аннушка', author: 'Толстой'},
+    {title: 'Преступление', author: 'Достоевский'},
+    {title: 'Комедия', author: 'Алигери'}
+  ];
+
+  createBook() {
+    this.books.push({
+      title: this.new_book.title,
+      author: this.new_book.author,
+    });
+  }
 }
