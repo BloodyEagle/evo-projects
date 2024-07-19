62afe698e1aa8630d3638e98ac662f1eb68406b2
BloodyEagle <bloodyeagle@gmail.com>
2024-07-19 20:26:12 +0300

    initial commit rxJS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {interval, map, Observable, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'First-evo-project';
+  orderIntervalSub$!: Subscription;
+  randomIntervalSub$!: Subscription;
+
+  enabledOrderCounter: boolean = false;
+  enabledRandomCounter: boolean = false;
+
+  orderArray: number[] = [];
+  randomArray: string[] = [];
+
+  constructor() {
+  }
+
+  public enableOrderStream() {
+    const orderStream = interval(2000);
+    this.orderIntervalSub$ = orderStream.subscribe((value) => {
+      this.orderArray.push(this.orderArray.length);
+    });
+    this.enabledOrderCounter = true;
+  }
+
+  public enableRandomStream() {
+    const randomStream = interval(2000);
+    this.randomIntervalSub$ = randomStream.pipe(
+      map((value) => `Random Value: ${Math.round(Math.random() * 100)}`)
+    )
+      .subscribe((value) => {
+        this.randomArray.push(value);
+      });
+    this.enabledRandomCounter = true;
+  }
+
+  public disableOrderStream() {
+    if (this.orderIntervalSub$) {
+      this.orderIntervalSub$.unsubscribe();
+    }
+    this.enabledOrderCounter = false;
+  }
+
+  public disableRandomStream() {
+    if (this.randomIntervalSub$) {
+      this.randomIntervalSub$.unsubscribe();
+    }
+    this.enabledRandomCounter = false;
+  }
 }
