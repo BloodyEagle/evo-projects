276fe0e243d58a981f8a100e47cc66ad3adeb5f4
BloodyEagle <bloodyeagle@gmail.com>
2024-07-26 14:39:33 +0300

    delete comments

--- src/app/post-edit.guard.ts
+++ src/app/post-edit.guard.ts
@@ -3,8 +3,6 @@ import {inject} from "@angular/core";
 import {PostService, Roles} from "./services/post.service";
 
 export const postEditGuard: CanActivateChildFn = (childRoute:ActivatedRouteSnapshot, state:RouterStateSnapshot) => {
-  //let nav = inject(Router).navigateByUrl('/error')
-  //console.log('nav=',nav.catch((error) => error));
   return inject(PostService).role === Roles.ADMIN
     ? true
     : inject(Router).parseUrl('/error');
